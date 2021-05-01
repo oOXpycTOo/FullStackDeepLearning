66b44e4cecb1338bfbded660b67ca0adb87ed359
uladkaz <vkazimirov@biquants.com>
2021-05-01 21:53:33 +0300

    Fixed inits. Updated model

--- lab3/text_recognizer/models/cnn.py
+++ lab3/text_recognizer/models/cnn.py
@@ -9,8 +9,9 @@ import torch.nn.functional as F
 CONV_DIM = 64
 FC_DIM = 128
 IMAGE_SIZE = 28
-DEFAULT_CONV_BLOCK = 'ConvBlock'
-DEFAULT_POOL_TYPE = 'ConvPool'
+DEFAULT_BACKBONE = 'resnet'
+DEFAULT_BLOCK = 'residual'
+DEFAULT_POOL_TYPE = 'max_pool'
 SE_REDUCTION_RATE = 2
 N_BLOCKS = 2
 
@@ -59,6 +60,7 @@ class ResidualBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(num_features=output_channels)
         if downsample:
             self.downsample = nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=res_stride)
+            self.bn_downsample = nn.BatchNorm2d(num_features=output_channels)
         else:
             self.downsample = None
 
@@ -77,6 +79,8 @@ class ResidualBlock(nn.Module):
         identity = x
         if self.downsample is not None:
             identity = self.downsample(identity)
+            identity = self.bn_downsample(identity)
+            identity = self.relu(identity)
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -85,23 +89,6 @@ class ResidualBlock(nn.Module):
         out = self.relu(out + identity)
         return out
 
-class GlobalAveragePooling(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Parameters
-        ----------
-        x
-            of dimensions (B, C_in, H, W)
-
-        Returns
-        -------
-        torch.Tensor
-            of dimensions (B, C_out, 1, 1)
-        """
-        return torch.mean(x, dim=(-1, -2))
 
 class SqueezeExcitationBlock(nn.Module):
     """
@@ -109,24 +96,27 @@ class SqueezeExcitationBlock(nn.Module):
     """
     def __init__(self, input_channels: int, output_channels: int, **kwargs: nn.Module) -> None:
         super().__init__()
-        self.residual_block: ResidualBlock = ResidualBlock(input_channels, output_channels, **kwargs)
+        downsample = kwargs.get('downsample', False)
         reduction_rate = kwargs.get('reduction_rate', SE_REDUCTION_RATE)
-        self.avg_pool = GlobalAveragePooling()
-        self.fc1 = nn.Linear(output_channels, output_channels // reduction_rate)
-        self.fc2 = nn.Linear(output_channels // reduction_rate, output_channels)
+        res_stride = 1
+        if downsample:
+            res_stride = 2
+        self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=res_stride, padding=1)
+        self.conv2 = nn.Conv2d(output_channels, output_channels, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_features=output_channels)
+        self.bn2 = nn.BatchNorm2d(num_features=output_channels)
+        if downsample:
+            self.downsample = nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=res_stride)
+            self.bn_downsample = nn.BatchNorm2d(num_features=output_channels)
+        else:
+            self.downsample = None
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc_se_1 = nn.Linear(output_channels, output_channels // reduction_rate)
+        self.fc_se_2 = nn.Linear(output_channels // reduction_rate, output_channels)
+
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
-
-    def _get_residual_and_identity(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        identity = x
-        if self.residual_block.downsample is not None:
-            identity = self.residual_block.downsample(identity)
-        out = self.residual_block.conv1(x)
-        out = self.residual_block.bn1(out)
-        out = self.residual_block.relu(out)
-        out = self.residual_block.conv2(out)
-        out = self.residual_block.bn2(out)
-        return out, identity
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -140,24 +130,82 @@ class SqueezeExcitationBlock(nn.Module):
         torch.Tensor
             of dimensions (B, C_out, H, W)
         """
-        res, identity = self._get_residual_and_identity(x)
-        channels_only = self.avg_pool(res)
-        channels_only = self.fc1(channels_only)
-        channels_only = self.relu(channels_only)
-        channels_only = self.fc2(channels_only)
-        res_weights = self.sigmoid(channels_only)
-        res_weights = res_weights.unsqueeze(-1).unsqueeze(-1)
-        res = res_weights * res
-        return res + identity
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+            identity = self.bn_downsample(identity)
+            identity = self.relu(identity)
+        residual = self.conv1(x)
+        residual = self.bn1(residual)
+        residual = self.relu(residual)
+        residual = self.conv2(residual)
+        residual = self.bn2(residual)
+
+        weights = self.avg_pool(residual).permute(0, 2, 3, 1)
+        weights = self.fc_se_1(weights)
+        weights = self.relu(weights)
+        weights = self.fc_se_2(weights)
+        weights = self.sigmoid(weights).permute(0, 3, 1, 2)
+
+        out = residual * weights
+        out = self.relu(out + identity)
+        return out
+
+
+class ResNetBackbone(nn.Module):
+    DOWNSAMPLE_EVERY = 2
+    BLOCKS = {'residual': ResidualBlock, 'squeeze_excitation': SqueezeExcitationBlock}
+    def __init__(self, input_size: int,
+                conv_dim: int,
+                n_blocks: int,
+                fc_dim: int,
+                block_type: str) -> None:
+        super().__init__()
+        conv_output_dim = conv_dim * 2 ** (n_blocks // self.DOWNSAMPLE_EVERY)
+        self.init_conv = nn.Conv2d(input_size, conv_dim, kernel_size=3, padding=1)
+        self.init_bn = nn.BatchNorm2d(conv_dim)
+        self.init_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.blocks = self._get_blocks(n_blocks, conv_dim, block_type)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.linear = nn.Linear(conv_output_dim, fc_dim)
+        self.relu = nn.ReLU()
+
+    def _get_blocks(self, n_blocks: int, conv_dim: int, block_type: str) -> torch.nn.ModuleList:
+        blocks = []
+        input_dim = conv_dim
+        output_dim = conv_dim
+        block_builder = self.BLOCKS[block_type]
+        for i in range(n_blocks):
+            if (i + 1) % self.DOWNSAMPLE_EVERY == 0:
+                downsample = True
+                output_dim *= 2
+            else:
+                downsample = False
+            blocks.append(block_builder(input_dim, output_dim, downsample=downsample))
+            input_dim = output_dim
+        return nn.ModuleList(blocks)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.init_conv(x)
+        x = self.init_bn(x)
+        x = self.relu(x)
+        x = self.init_pool(x)
+        for block in self.blocks:
+            x = block(x)
+        x = self.avg_pool(x).squeeze(-1).squeeze(-1)
+        x = self.linear(x)
+        x = self.relu(x)
+        return x
 
 
 class CNN(nn.Module):
     """Simple CNN for recognizing characters in a square image."""
-    CONV_BLOCKS = {'ConvBlock': ConvBlock, 'ResidualBlock': ResidualBlock, 'SEBlock': SqueezeExcitationBlock}
+    BACKBONES = {'lenet': ConvBlock, 'resnet': ResNetBackbone}
 
     def __init__(self, data_config: Dict[str, Any], args: argparse.Namespace = None) -> None:
         super().__init__()
         self.args = vars(args) if args is not None else {}
+
 
         input_dims = data_config["input_dims"]
         num_classes = len(data_config["mapping"])
@@ -165,14 +213,10 @@ class CNN(nn.Module):
         conv_dim = self.args.get("conv_dim", CONV_DIM)
         fc_dim = self.args.get("fc_dim", FC_DIM)
 
-        conv_block = self.args.get("conv_block", DEFAULT_CONV_BLOCK)
-        instantiator = self.CONV_BLOCKS[conv_block]
-
+        backbone = self.args.get("backbone", DEFAULT_BACKBONE)
         n_blocks = self.args.get("n_blocks", N_BLOCKS)
-        pool_type = self.args.get("pool_type", DEFAULT_POOL_TYPE)
-
-        self.conv1 = ConvBlock(input_dims[0], conv_dim)
-        self.blocks = self._get_blocks(n_blocks, conv_dim, instantiator)
+        block_type = self.args.get("block_type", DEFAULT_BLOCK)
+        self.backbone = self.BACKBONES[backbone](input_dims[0], conv_dim, n_blocks, fc_dim, block_type)
 
         use_dropout = self.args.get("use_dropout")
         if use_dropout:
@@ -180,29 +224,7 @@ class CNN(nn.Module):
         else:
             self.dropout = nn.Identity()
 
-        self.pooling = self._get_pooling(pool_type, conv_dim)
-
-        # Because our 3x3 convs have padding size 1, they leave the input size unchanged.
-        # The 2x2 max-pool divides the input size by 2. Flattening squares it.
-        conv_output_size = IMAGE_SIZE
-        for _ in range(n_blocks // 2 + 1):
-            conv_output_size = conv_output_size // 2
-        fc_input_dim = int(conv_output_size * conv_output_size * conv_dim)
-        self.fc1 = nn.Linear(fc_input_dim, fc_dim)
-        self.fc2 = nn.Linear(fc_dim, num_classes)
-
-    def _get_blocks(self, n_blocks: int, conv_dim: int, instantiator: nn.Module) -> torch.nn.ModuleList:
-        blocks = []
-        for i in range(n_blocks):
-            downsample = True if (i + 1) % 2 == 0 else False
-            blocks.append(instantiator(conv_dim, conv_dim, downsample=downsample))
-        return nn.ModuleList(blocks)
-        
-    def _get_pooling(self, pool_type: str, dim: int):
-        if pool_type == 'MaxPool':
-            return nn.MaxPool2d(2)
-        elif pool_type == 'ConvPool':
-            return nn.Conv2d(dim, dim, kernel_size=1, stride=2, bias=False)
+        self.out_fc = nn.Linear(fc_dim, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -217,23 +239,17 @@ class CNN(nn.Module):
         """
         _B, _C, H, W = x.shape
         assert H == W == IMAGE_SIZE
-        x = self.conv1(x)
-        for block in self.blocks:
-            x = block(x)
-        x = self.pooling(x)
+        x = self.backbone(x)
         x = self.dropout(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
+        x = self.out_fc(x)
         return x
 
     @staticmethod
     def add_to_argparse(parser):
         parser.add_argument("--conv_dim", type=int, default=CONV_DIM)
         parser.add_argument("--fc_dim", type=int, default=FC_DIM)
-        parser.add_argument("--conv_block", type=str, default=DEFAULT_CONV_BLOCK)
-        parser.add_argument("--pool_type", type=str, default=DEFAULT_POOL_TYPE)
+        parser.add_argument("--backbone", type=str, default=DEFAULT_BACKBONE)
+        parser.add_argument("--block_type", type=str, default=DEFAULT_BLOCK)
         parser.add_argument("--n_blocks", type=int, default=N_BLOCKS)
         parser.add_argument("--use_dropout", action='store_true')
         return parser
